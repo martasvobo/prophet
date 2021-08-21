671e0e18df05d659e0409a2f501392ca85bf184b
martasvobo <martasvobo@gmail.com>
2021-08-21 12:33:19 +0300

    ending...

--- script.js
+++ script.js
@@ -3,5 +3,15 @@ const days = [['today', 'day'], ['tomorrow'], ['day'], ['next week', 'week'],
 const inThe = ['will be', 'will not be'];
 const future = ['the best', 'the worst'];
 
-console.log('Prophet says that ');
+console.log('Prophet says that ' + prophecy(n1, n2, n3) + ' in your life.');
 
+const n1, n2, n3;
+n1 = Math.floor(Math.random() * days.length);
+n2 = Math.floor(Math.random() * inThe.length);
+n3 = Math.floor(Math.random() * future.length);
+
+function prophecy(n1, n2, n3)
+{
+    console.log(days[n1, 0] + ' ' + inThe[n2] + ' ' + future[n3] +
+    ' ' + days[n1, 1]);
+}
