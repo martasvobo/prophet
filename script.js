5317f2bc9ff1f57e4c5812bace06804d5afb183c
martasvobo <martasvobo@gmail.com>
2021-08-21 12:47:44 +0300

    final

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-const days = [['today', 'day'], ['tomorrow'], ['day'], ['next week', 'week'],
+const days = [['today', 'day'], ['tomorrow', 'day'], ['next week', 'week'],
 ['next month', 'month'], ['next year', 'year']];
 const inThe = ['will be', 'will not be'];
 const future = ['the best', 'the worst'];
 
-console.log('Prophet says that ' + prophecy(n1, n2, n3) + ' in your life.');
-
-const n1, n2, n3;
-n1 = Math.floor(Math.random() * days.length);
-n2 = Math.floor(Math.random() * inThe.length);
-n3 = Math.floor(Math.random() * future.length);
-
+const n1 = Math.floor(Math.random() * days.length);
+const n2 = Math.floor(Math.random() * inThe.length);
+const n3 = Math.floor(Math.random() * future.length);
 function prophecy(n1, n2, n3)
 {
-    console.log(days[n1, 0] + ' ' + inThe[n2] + ' ' + future[n3] +
-    ' ' + days[n1, 1]);
+    let str = (days[n1][0] + ' ' + inThe[n2] + ' ' + future[n3] +
+    ' ' + days[n1][1]);
+    return str;
 }
+
+console.log('Prophet says that ' + prophecy(n1, n2, n3) + ' in your life.');
